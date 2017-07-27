dfa314d138b8e0079572c177bd117ae3eb2ee314
Samuel Albert <samuel.albert@centraliens.net>
2017-07-27 20:09:10 +0200

    Giving the player the starting items
    Signed-off-by: Samuel Albert <samuel.albert@centraliens.net>

--- scenarios/test-TAS/control.lua
+++ scenarios/test-TAS/control.lua
@@ -4,6 +4,12 @@ require("configuration")
 local commandqueue = require(run_file)
 
 script.on_event(defines.events.on_player_created, function(event)
+	local myplayer = game.players[1]
+	myplayer.insert{name="iron-plate", count=8}
+	myplayer.insert{name="pistol", count=1}
+	myplayer.insert{name="firearm-magazine", count=10}
+	myplayer.insert{name="burner-mining-drill", count = 1}
+	myplayer.insert{name="stone-furnace", count = 1}
 	if autorun then
 		remote.call("TAS_playback", "launch", commandqueue)
 	end
