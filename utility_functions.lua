0b5abefa03ce21021ff53ab874fc020181b90906
Bilka2 <bilka.milli@hotmail.de>
2017-09-18 17:54:52 +0200

    Pretty time option

--- utility_functions.lua
+++ utility_functions.lua
@@ -10,15 +10,18 @@ end
 
 local function prettytime()
   local tick = game.tick - (global.start_tick or 0)
-  local hours = string.format("%02.f", math.floor(tick / 216000))
-  local minutes = string.format("%02.f", math.floor(tick / 3600) - hours * 60)
-  local seconds = string.format("%02.f", math.floor(tick / 60) - hours * 3600 - minutes * 60)
-  local ticks = string.format("%02.f", tick - hours * 216000 - minutes * 3600 - seconds * 60)
-  if hours == "00" then
-    return "[" .. minutes .. ":" .. seconds .. ":" .. ticks .. "] "
-  else
-    return "[" .. hours .. ":" .. minutes .. ":" .. seconds .. ":" .. ticks .. "] "
+  if settings.global["tas-pretty-time"].value then
+    local hours = string.format("%02.f", math.floor(tick / 216000))
+    local minutes = string.format("%02.f", math.floor(tick / 3600) - hours * 60)
+    local seconds = string.format("%02.f", math.floor(tick / 60) - hours * 3600 - minutes * 60)
+    local ticks = string.format("%02.f", tick - hours * 216000 - minutes * 3600 - seconds * 60)
+    if hours == "00" then
+      return "[" .. minutes .. ":" .. seconds .. ":" .. ticks .. "] "
+    else
+      return "[" .. hours .. ":" .. minutes .. ":" .. seconds .. ":" .. ticks .. "] "
+    end
   end
+  return "[" .. tick .. "] "
 end
 
 function debugprint(msg)
